91e0f0b936d7873ad67927a6e013889024f5b276
Mehmet Guler <mehmetgspace@gmail.com>
2022-07-31 18:16:43 +0300

    Add createPermalink function

---
+++ moon.js
@@ -0,0 +1,25 @@
+/**
+ * Create permalink
+ * @param text
+ * @returns {string}
+ */
+function createPermalink(text) {
+  if (typeof text === 'string') {
+    text = text.replace(new RegExp('Ç', 'g'), 'c');
+    text = text.replace(new RegExp('ç', 'g'), 'c');
+    text = text.replace(new RegExp('Ğ', 'g'), 'g');
+    text = text.replace(new RegExp('ğ', 'g'), 'g');
+    text = text.replace(new RegExp('I', 'g'), 'i');
+    text = text.replace(new RegExp('ı', 'g'), 'i');
+    text = text.replace(new RegExp('İ', 'g'), 'i');
+    text = text.replace(new RegExp('Ö', 'g'), 'o');
+    text = text.replace(new RegExp('ö', 'g'), 'o');
+    text = text.replace(new RegExp('Ş', 'g'), 's');
+    text = text.replace(new RegExp('ş', 'g'), 's');
+    text = text.replace(new RegExp('Ü', 'g'), 'u');
+    text = text.replace(new RegExp('ü', 'g'), 'u');
+    text = text.replace(new RegExp(' ', 'g'), '-');
+  }
+
+  return text.toLowerCase();
+}
